27a5fee0cbeadaa72eba3e6e8023d1ac12fe9593
Erich Richter <erich.richter@gmail.com>
2020-01-31 16:00:09 -0800

    add price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,35 @@ $(document).ready(function(){
     
     
     if  (document.getElementById('pepperoni').checked) {
-     
- 
+      
        var topping1 = "pepperoni"
        var topping1 = new Topping(topping1);
-       
        pizza.addToppings(topping1);
-    }
+      
+       var price = new Price(price);
+       pizza.addPrices(price);
+      
+      }
     
        console.log(pizza)
       
      
       if (document.getElementById('mushrooms').checked){
-    
         var topping2 = "mushrooms"
         var topping2 = new Topping(topping2);
-        
         pizza.addToppings(topping2);
-        
+       
       }
+    
+    
+    if (document.getElementById('small').checked){
+
+     
+    }
+    
+    
+    
+    
     
     });
 
@@ -39,15 +49,25 @@ $(document).ready(function(){
 
 function Pizza() {
   this.toppings = [];
+  this.prices = [];
 }
 
 Pizza.prototype.addToppings = function(topping) {
   this.toppings.push(topping);
 }
+Pizza.prototype.addPrices = function(price) {
+  
+  var price = "5"
+  this.prices.push(price);
+}
+
 // Business Logic for Toppings 
 function Topping(topping) {
   this.topping = topping;
-  
+}
+  // Business Logic for price
+function Price(price) {
+  this.price = price;
 }
 
 
